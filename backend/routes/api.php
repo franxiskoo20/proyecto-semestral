be9bfa1e6d222aef5bd31f399b1e419fd51e8fcd
Valdieggo <diego.valdera98@gmail.com>
2023-12-20 03:32:19 -0300

    Bodega y Cargamento  controllers/ API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
-
+use App\Http\Controllers\CargamentoController;
+use App\Http\Controllers\BodegaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -37,4 +38,24 @@ POST /productos: Crear un nuevo producto.
 PUT /productos/{id}: Actualizar un producto.
 DELETE /productos/{id}: Eliminar un producto.
 PUT /productos/restore/{id}: Restaurar un producto eliminado. 
+*/
+
+Route::group(['prefix' => 'cargamentos'], function(){
+    Route::get('/', [CargamentoController::class, 'index']);
+    Route::post('/', [CargamentoController::class, 'crear']);
+});
+
+/* 
+GET /cargamentos: Obtener todos los cargamentos.
+POST /cargamentos: Crear un nuevo cargamento. 
+*/
+
+Route::group(['prefix' => 'bodegas'], function(){
+    Route::get('/', [BodegaController::class, 'index']);
+    Route::post('/', [BodegaController::class, 'crear']);
+});
+
+/*
+GET /bodegas: Obtener todas las bodegas.
+POST /bodegas: Crear una nueva bodega. 
 */
